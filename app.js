01440a1bcabeebb997c3d2305aeac6626228c439
jamesbowd <jamesbowd@gmail.com>
2020-05-04 16:33:11 +0100

    grid options

--- app.js
+++ app.js
@@ -1,36 +1,83 @@
-const square = document.querySelectorAll(".square");
 const mole = document.querySelectorAll(".mole");
 const timeLeft = document.querySelector("#time-left");
+const grid = document.querySelector(".grid");
 let score = document.querySelector("#score");
+const gridSize = document.querySelectorAll(".grid-size-btn");
+
+let squares;
+let square;
 
 let result = 0;
 let currentTime = timeLeft.textContent;
+
+let timerID = null;
+
+for (const button of gridSize) {
+  button.addEventListener("click", setGridSize);
+}
+
+function init() {
+  grid.innerHTML = "";
+  score.textContent = "0";
+  timeLeft.textContent = "60";
+}
+
+function setGridSize() {
+  init();
+  if (this.getAttribute("id") == "three-by-three") {
+    squares = 9;
+    grid.style.width = "30vw";
+    createBoard();
+  }
+  if (this.getAttribute("id") == "four-by-three") {
+    squares = 12;
+    grid.style.width = "40vw";
+    createBoard();
+  }
+  if (this.getAttribute("id") == "five-by-three") {
+    squares = 15;
+    grid.style.width = "50vw";
+    createBoard();
+  }
+  square = document.querySelectorAll(".square");
+  click();
+  moveMole();
+}
+
+function createBoard() {
+  for (let i = 0; i < squares; i++) {
+    let createSquare = document.createElement("div");
+    createSquare.setAttribute("id", i);
+    createSquare.setAttribute("class", "square");
+    grid.appendChild(createSquare);
+  }
+}
 
 function randomSquare() {
   square.forEach((className) => {
     className.classList.remove("mole");
   });
-  let randomPosition = square[Math.floor(Math.random() * 9)];
+  let randomPosition = square[Math.floor(Math.random() * squares)];
   randomPosition.classList.add("mole");
-
   hitPosition = randomPosition.id;
 }
 
-square.forEach((id) => {
-  id.addEventListener("mouseup", () => {
-    if (id.id === hitPosition) {
-      result = result + 1;
-      score.textContent = result;
-    }
+function click() {
+  square.forEach((id) => {
+    id.addEventListener("mouseup", () => {
+      if (id.id === hitPosition) {
+        result = result + 1;
+        score.textContent = result;
+      }
+    });
   });
-});
-
-function moveMole() {
-  let timerID = null;
-  timerID = setInterval(randomSquare, 1000);
 }
 
-moveMole();
+function moveMole() {
+  //let timerID = null;
+  timerID = setInterval(randomSquare, 1000);
+  timerID = setInterval(countDown, 1000);
+}
 
 function countDown() {
   currentTime--;
@@ -41,4 +88,4 @@ function countDown() {
   }
 }
 
-let timerID = setInterval(countDown, 1000);
+//timerID = setInterval(countDown, 1000);
